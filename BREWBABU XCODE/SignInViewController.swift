65d1340171f251b43797f99a73aac6e77d2be2bd
LAW <DAD@LAWs-MacBook-Pro.local>
2017-08-24 16:29:31 +0100

    Completed setting up firebase intergration

---
+++ BREWBABU XCODE/SignInViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BREWBABU XCODE
+//
+//  Created by LAW on 24/08/2017.
+//  Copyright © 2017 LAW. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-BREWBABU XCODE/Pods-BREWBABU XCODE-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BREWBABU_XCODE : NSObject
+@end
+@implementation PodsDummy_Pods_BREWBABU_XCODE
+@end
