8ff3c7c1ed3e54333b22e795ab7c88d393e3635f
LAW <DAD@LAWs-MacBook-Pro.local>
2017-08-24 20:20:46 +0100

    Sign In UI Custom Classes Complete

---
+++ BREWBABU XCODE/FancyView.swift
@@ -0,0 +1,21 @@
+//
+//  FancyView.swift
+//  BREWBABU XCODE
+//
+//  Created by LAW on 24/08/2017.
+//  Copyright © 2017 LAW. All rights reserved.
+//
+
+import UIKit
+
+class FancyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ BREWBABU XCODE/RoundedSignInButton.swift
@@ -0,0 +1,20 @@
+//
+//  RoundedSignInButton.swift
+//  BREWBABU XCODE
+//
+//  Created by LAW on 24/08/2017.
+//  Copyright © 2017 LAW. All rights reserved.
+//
+
+import UIKit
+
+class RoundedSignInButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.cornerRadius = 25
+        
+    }
+
+}
